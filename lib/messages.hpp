ee933c0acc44d61bd97f669009a905c9d8a5ff3d
lizardo-andres12 <lizardo.andres12@gmail.com>
2025-11-19 03:43:58 -0500

    add: feed message type reprs

---
+++ lib/messages.hpp
@@ -0,0 +1,35 @@
+#ifndef MESSAGES
+#define MESSAGES
+
+#include <cstdint>
+
+
+struct __attribute__((packed)) TradeMessage {
+    std::uint8_t type;
+    std::uint64_t timestamp;
+    std::uint64_t symbol;
+    std::uint64_t price;
+    std::uint32_t quantity;
+    std::uint8_t padding[3];
+};
+
+struct __attribute__((packed)) QuoteMessage {
+    std::uint8_t type;
+    std::uint64_t timestamp;
+    std::uint64_t symbol;
+    std::uint64_t bidPrice;
+    std::uint32_t bidQuantity;
+    std::uint64_t askPrice;
+    std::uint32_t askQuantity;
+    std::uint8_t padding[3];
+};
+
+union MarketDataMessage {
+    std::uint8_t type;
+    TradeMessage trade;
+    QuoteMessage quote;
+};
+
+
+#endif
+
