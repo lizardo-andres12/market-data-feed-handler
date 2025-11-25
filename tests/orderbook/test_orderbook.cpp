954532a1b24ecd83fa5fc15499518e8aaab887c8
lizardo-andres12 <lizardo.andres12@gmail.com>
2025-11-25 15:21:08 -0500

    add: tests directory with sample test

---
+++ tests/orderbook/test_orderbook.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+//#include "orderbook/orderbook.hpp"
+
+
+TEST(SystemTest, SanityCheck) {
+    EXPECT_EQ(1 + 1, 2);
+}
+
+TEST(OrderbookTest, CanInstantiate) {
+    EXPECT_TRUE(true);
+}
+
