be423de6a86a1e27330584fa8589651e110d714c
lizardo-andres12 <lizardo.andres12@gmail.com>
2025-11-27 02:17:02 -0500

    refactor: change `getEntry` param and nest `OrderBookEntry`

--- include/orderbook/orderbook.hpp
+++ include/orderbook/orderbook.hpp
@@ -1,24 +1,10 @@
-#ifndef ORDERBOOK_HPP
-#define ORDERBOOK_HPP
+#pragma once
 
 #include <cstdint>
 #include <optional>
 #include <unordered_map>
 
-/// type alias for clarity
-using __book_key_t = std::uint64_t;
-
-
-/**
- * @brief The collection of relevant data to store in the OrderBook for a single symbol.
- */
-struct OrderBookEntry {
-    std::uint64_t udpatedAt;
-    std::uint64_t bidPrice;
-    std::uint64_t askPrice;
-    std::uint32_t bidQuantity;
-    std::uint32_t askQuantity;
-};
+#include "messages.hpp"
 
 
 /**
@@ -27,22 +13,51 @@ struct OrderBookEntry {
  * data structure cannot and should not be moved or copied.
  */
 class OrderBook {
-    std::unordered_map<__book_key_t, OrderBookEntry> book_;
 public:
+    /**
+     * @brief The collection of relevant data to store in the OrderBook for a single symbol.
+     */
+    struct OrderBookEntry {
+	std::uint64_t udpatedAt;
+	std::uint64_t bidPrice;
+	std::uint64_t askPrice;
+	std::uint32_t bidQuantity;
+	std::uint32_t askQuantity;
+    };
+
+    /**
+     * @brief Default constructor for OrderBook that instantiates all member variables.
+     */
     OrderBook() = default;
 
-    std::optional<const OrderBookEntry*> getEntry(const __book_key_t key) const;
+    /**
+     * @brief Attempts to retrieve the order metadata object associated with the input symbol.
+     *
+     * @param key The symbol of the instrument stored as the key.
+     * @return An optional containing a const pointer to the `OrderBookEntry` associated with `key` if it exists.
+     */
+    std::optional<const OrderBookEntry*> getEntry(const std::uint64_t key) const;
 
-    void upsertEntry(const __book_key_t key, OrderBookEntry& entry);
+    /**
+     * @brief Creates a new symbol-to-quote mapping using the symbol and `QuoteMessage` object provided. If the key already exits,
+     * 		map the key to a new entry based on `msg`.
+     *
+     * @param key The symbol to update or insert.
+     * @param msg The data of the incoming quote.
+     */
+    void upsertEntry(const std::uint64_t, const QuoteMessage& msg);
 
+    /**
+     * @brief Displays the state of the book.
+     */
     void showState() const;
 
     OrderBook(const OrderBook& ob) = delete;
     OrderBook(OrderBook&& ob) = delete;
     void operator=(const OrderBook& ob) = delete;
     void operator=(OrderBook&& ob) = delete;
+
+private:
+    std::unordered_map<std::uint64_t, OrderBookEntry> book_;
 };
-
-
-#endif
 
